d015cbeabe76565999d9a313e30310154a59d373
Interdisciplinary Physics Team (InPhyT) <inphyt@gmail.com>
2022-05-09 18:00:26 +0200

    Create MultilayerGraphs.jl
    Co-Authored-By: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
    Co-Authored-By: Claudio Moroni <43729990+ClaudMor@users.noreply.github.com>

---
+++ docs/make.jl
@@ -0,0 +1,26 @@
+using MultilayerGraphs
+using Documenter
+
+DocMeta.setdocmeta!(MultilayerGraphs, :DocTestSetup, :(using MultilayerGraphs); recursive=true)
+
+makedocs(;
+    modules=[MultilayerGraphs],
+    authors="Pietro Monticone, Claudio Moroni",
+    repo="https://github.com/InPhyT/MultilayerGraphs.jl/blob/{commit}{path}#{line}",
+    sitename="MultilayerGraphs.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://InPhyT.github.io/MultilayerGraphs.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home"      => "index.md",
+        "Internals" => "internals.md"
+    ],
+)
+
+deploydocs(;
+    repo="github.com/InPhyT/MultilayerGraphs.jl",
+    devbranch="main",
+    push_preview = true
+)
---
+++ docs/src/index.md
@@ -0,0 +1,20 @@
+```@meta
+CurrentModule = MultilayerGraphs
+```
+
+```@raw html
+<div style="width:100%; height:150px;border-width:4px;border-style:solid;padding-top:25px;
+        border-color:#000;border-radius:10px;text-align:center;background-color:#B3D8FF;
+        color:#000">
+    <h3 style="color: black;">Star us on GitHub!</h3>
+    <a class="github-button" href="https://github.com/InPhyT/MultilayerGraphs.jl" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star InPhyT/MultilayerGraphs.jl on GitHub" style="margin:auto">Star</a>
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+</div>
+```
+
+# MultilayerGraphs
+
+```@contents
+```
+
+## Installation
