0b032baf2fdc52d0b44f6fd2f6c0b80828629ec2
Interdisciplinary Physics Team (InPhyT) <63546437+InterdisciplinaryPhysicsTeam@users.noreply.github.com>
2022-12-12 15:44:10 +0100

    Update docstrings
    Co-Authored-By: Claudio Moroni <43729990+ClaudMor@users.noreply.github.com>
    Co-Authored-By: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>

--- src/graphofgraphs/abstractgraphofgraphs.jl
+++
@@ -1,6 +0,0 @@
-"""
-    abstract type AbstractGraphOfGraphs{T <: AbstractGraph} <: AbstractGraph{T} end
-
-An abstract type representing a graph of graphs.
-"""
-abstract type AbstractGraphOfGraphs{T<:AbstractGraph} <: AbstractGraph{T} end
