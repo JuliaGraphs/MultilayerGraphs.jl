77e962dedd859d64a6bbb028404d91a288b3a9a2
Interdisciplinary Physics Team (InPhyT) <63546437+InterdisciplinaryPhysicsTeam@users.noreply.github.com>
2022-12-14 12:59:27 +0100

    Test `isdigraphical`
    Co-Authored-By: Claudio Moroni <43729990+ClaudMor@users.noreply.github.com>
    Co-Authored-By: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>

--- test/utilities.jl
+++ test/utilities.jl
@@ -3,3 +3,5 @@ const A = reshape(rand(1:9, 1000), 10, 10, 10)
 carts_idxs = CartesianIndices(A)
 vec_idxs = MultilayerGraphs.cartIndexTovecIndex.(carts_idxs, Ref(size(A)))
 @test all(getindex.(Ref(A), carts_idxs) .== getindex.(Ref(A), vec_idxs)) 
+directed_graphs = [SimpleDiGraph(10,50) for i in 1:5]
+@test all(MultilayerGraphs.isdigraphical.(indegree.(directed_graphs), outdegree.(directed_graphs)))
