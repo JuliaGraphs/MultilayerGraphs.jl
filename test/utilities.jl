7c4edc863a93c07b48edcc858cb5762446ee6e98
Interdisciplinary Physics Team (InPhyT) <63546437+InterdisciplinaryPhysicsTeam@users.noreply.github.com>
2022-12-14 01:01:17 +0100

    Update test
    Co-Authored-By: Claudio Moroni <43729990+ClaudMor@users.noreply.github.com>
    Co-Authored-By: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>

--- test/utilities.jl
+++ test/utilities.jl
@@ -2,9 +2,4 @@
 const A = reshape(rand(1:9, 1000), 10, 10, 10)
 carts_idxs = CartesianIndices(A)
 vec_idxs = MultilayerGraphs.cartIndexTovecIndex.(carts_idxs, Ref(size(A)))
-@test all(getindex.(Ref(A), carts_idxs) .== getindex.(Ref(A), vec_idxs) ) 
-# Test directed simple graphicality
-sdg = SimpleDiGraph(10, 90)
-@test @inferred(isdigraphical(indegree(sdg), outdegree(sdg)))
-@test !@inferred(isdigraphical([1, 1, 1], [1, 1, 0]))
-@test @inferred(isdigraphical(Integer[], Integer[]))
+@test all(getindex.(Ref(A), carts_idxs) .== getindex.(Ref(A), vec_idxs)) 
