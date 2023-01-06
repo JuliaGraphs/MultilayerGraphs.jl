2c4ffe8d6c1fe83eb985b805c452d1ecfecec0a5
Interdisciplinary Physics Team (InPhyT) <63546437+InterdisciplinaryPhysicsTeam@users.noreply.github.com>
2023-01-06 15:27:21 +0100

    Update example_complex.jl
    Co-Authored-By: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
    Co-Authored-By: Claudio Moroni <43729990+ClaudMor@users.noreply.github.com>

--- paper/example_complex.jl
+++ paper/example_complex.jl
@@ -4,8 +4,8 @@ using Distributions
 using MultilayerGraphs
 
 # The objects that `MultilayerVertex`s represent are `Node`s
-const n_nodes = 100
-const nodes = [Node("node_$i") for i in 1:n_nodes]
+const n__nodes = 100
+const _nodes = [Node("node_$i") for i in 1:n__nodes]
 
 #= # Every layer and Interlayer that is involved in a `Multilayer(Di)Graph` must have the same vertex type (i.e. the type of the internal representation of vertices) and (edge) weight type. We proceed to define them hereafter:
 const vertex_type = Int64
@@ -16,30 +16,30 @@ const weight_type = Float64 =#
 _nv = rand(1:100)
 _layer_simpledigraph = layer_simpledigraph(
     :layer_simpledigraph,           # The name of the layer_metagraph
-    sample(nodes, _nv; replace=false),                          # The nodes that the layer will represent
+    sample(_nodes, _nv; replace=false),                          # The _nodes that the layer will represent
     Truncated(Normal(5, 5), 0, 20),  # The distribution from which the indegree sequence will be sampled from
     Truncated(Normal(5, 5), 0, 20),   # The distribution from which the outdegree sequence will be sampled from
 )
 
 ## Next, we define a layer with an underliyng simple weighted directed graph. This time we show another kind of constructor that allows the user to specify the number of edges to be randomly distributed among the vertices. The keyword argument `default_edge_weight` will assign a weight to such edges before they are added to the layer
-_nv = rand(1:n_nodes)
+_nv = rand(1:n__nodes)
 _ne = rand(_nv:(_nv * (_nv - 1) - 1))
 _layer_simpleweighteddigraph = layer_simpleweighteddigraph(
     :layer_simpleweighteddigraph,
-    sample(nodes, _nv; replace=false),
+    sample(_nodes, _nv; replace=false),
     _ne;                                      # The number of edges that will be randomly distributed among the vertices
     default_edge_weight=(src, dst) -> rand(), # The function that assigns a weight to such edges before they are added to the layer
 )
 
 ## Similar constructors, more flexible at the cost of ease of use, allows for finer tuning:
 ### NB: This constructor should be necessary only in rare circumstances, where e.g. the equivalent simplified constructor `layer_simplevaldigraph` is not able to infer the correct return types of the `default_vertex/edge_metadata`s, or to use underlying graph that aren't currently supported.
-_nv = rand(1:n_nodes)
+_nv = rand(1:n__nodes)
 _ne = rand(_nv:(_nv * (_nv - 1) - 1))
 default_vertex_metadata = v -> ("vertex_$(v)_metadata",)
 default_edge_metadata = (s, d) -> (rand(),)
 _layer_valdigraph = Layer(
     :layer_simplevaldigraph,
-    sample(nodes, _nv; replace=false),
+    sample(_nodes, _nv; replace=false),
     _ne,
     ValDiGraph(
         SimpleDiGraph{Int64}();
@@ -70,8 +70,8 @@ _interlayer_simpledigraph = interlayer_simpledigraph(
 )
 
 ## The interlayer exports a more flexible constructor too. 
-nv_1 = nv(_layer_simpledigraph)
-nv_2 = nv(_layer_simpleweighteddigraph)
+nv_1 = nv(_layer_simpleweighteddigraph)
+nv_2 = nv(_layer_valdigraph)
 _ne = rand(1:(nv_1 * nv_2 - 1))
 _interlayer_metadigraph = interlayer_metadigraph(
     _layer_simpleweighteddigraph,
@@ -93,9 +93,12 @@ multilayerdigraph = MultilayerDiGraph(
     default_interlayers_structure="multiplex", # The automatically-specified interlayers will have only diagonal couplings
 )
 
+# Layers and interlayer may be accessed as properties using their names
+multilayerdigraph.layer_simplevaldigraph
+
 # We proceed to show some basic funcionality.
 ## Add a vertex
-### The user may add vertices that do or do not represent nodes already present in the multilayergraph. In the latter case, we have a new node:
+### The user may add vertices that do or do not represent _nodes already present in the multilayergraph. In the latter case, we have a new node:
 new_node_1 = Node("new_node_1")
 ### Before adding any vertex representing such node to the multilayer graph, the user should first add the Node:
 add_node!(multilayerdigraph, new_node_1)
@@ -103,21 +106,55 @@ add_node!(multilayerdigraph, new_node_1)
 new_vertex_1 = MV(                         # MV is an alias for MultilayerVertex
     new_node_1,              # The Node that the vetex represents 
     :layer_simplevaldigraph, # The layer which the vertex belongs to
-    ("new_metadata"),        # Metadata to associate to the vertex. Note that vertex- (or edge-) level metadata are considered iff the layer/interlayer supports them (i.e. if the graph underlying the subgraph supports them), otherwise they are discarded.
+    ("new_metadata",),        # Metadata to associate to the vertex.
 )
 ### Add the vertex
 add_vertex!(
     multilayerdigraph,  # The MultilayerDiGraph which the vertex will be added to
-    new_vertex_1,        # The MultilayerVertex to add
+    new_vertex_1,       # The MultilayerVertex to add. Note that vertex-level metadata are considered iff the graph underlying the layer/interlayer which the vertex belongs to supports them, otherwise they are discarded.
 )
 ### `add_vertex!` implements multiple interfaces.
 
 ## Add an edge
 ### Let's represent another node in another layer
 new_node_2 = Node("new_node_2")
+new_vertex_2 = MV(new_node_2, :layer_simpledigraph)
 add_vertex!(
     multilayerdigraph,
-    MV(new_node_2, :layer_simpledigraph);
+    new_vertex_2;
     add_node=true,                                # Also perform `add_node!(new_node_2)` before adding the vertex
 )
-### Let' add an edge
+### Construct a new edge
+new_edge = MultilayerEdge(
+    new_vertex_1,           # The source of the edge
+    new_vertex_2,           # The destination of the edge
+    ("some_edge_metadata",), # Metadata to associate to the edge. 
+)
+### Add the edge
+add_edge!(
+    multilayerdigraph, # The MultilayerDiGraph which the edge will be added to
+    new_edge,           # The MultilayerVertex to add. Note that edge-level metadata and/or weight are considered iff the graph underlying the layer/interlayer which the edge belongs to supports them, otherwise they are discarded.
+)
+
+# Using the provided `add_layer!`, `rem_layer!` and `specify_interlayer!`, Layers and Interlayers may be added, removed or specified on the fly. 
+
+# Since MultilayerGraphs.jl extends Graphs.jl, all metrics from the JuliaGraphs ecosystem should be avaliable by default. Anyway, some  Multilayer graph-specific metrics have been implemented, and others required to be re-implemented.  A few of them are shown  hereafter
+
+## Compute the global clustering coefficient as in @DeDomenico2014
+multilayer_global_clustering_coefficient(multilayerdigraph) # A weighted version `multilayer_weighted_global_clustering_coefficient`, from the same authors ad been implemented
+
+## Compute the overlay clustering coefficient as in @DeDomenico2014
+overlay_clustering_coefficient(multilayerdigraph)
+
+## Compute the eigenvector centality (the implementaation is sp that it coincides with Graphs.jl's `eigenvector_centality` on monoplex graphs)
+eigenvector_centrality(multilayerdigraph)
+
+## Compute the multilayer modularity as in @DeDomenico2014
+modularity(
+    multilayerdigraph,
+    rand([1, 2, 3, 4], length(nodes(multilayerdigraph)), length(multilayerdigraph.layers)),
+)
+
+## Currently, Von Neumann entropy is available only for undirected multilayer graphs.
+
+# NB: this brief script is far from complete: many more features and funcionalities are detailed in the documentation.
